5ee8871bdc9828f351aa4588b89ae12bceee76dd
somebodyfxq-stack <somebodyfxq@gmail.com>
2025-08-13 22:42:25 +0300

    chore: ignore system directories in dev watch

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,18 @@
-export default { reactStrictMode: true };
+function webpackDevMiddleware(config) {
+  config.watchOptions = {
+    ...config.watchOptions,
+    ignored: [
+      '**/node_modules/**',
+      '**/$RECYCLE.BIN/**',
+      '**/System Volume Information/**'
+    ]
+  };
+}
+
+export default {
+  reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (dev) webpackDevMiddleware(config);
+    return config;
+  }
+};
